229b696989358788d7b554ad4ad5dabc709ca339
Marcelo Sercheli <marcelo.sercheli@hernehill.com>
2025-05-28 16:15:16 -0400

    Initial commit

---
+++ package.py
@@ -0,0 +1,31 @@
+name = 'jwt_cpp'
+
+version = '0.7.1.hh.1.0.0'
+
+authors = [
+    'Dominik Thalhammer',
+]
+
+description = '''JWT C++'''
+
+with scope('config') as c:
+    import os
+    c.release_packages_path = os.environ['HH_REZ_REPO_RELEASE_EXT']
+    c.plugins.release_hook.hh_emailer.recipients = []
+
+requires = [
+    "json",
+]
+
+private_build_requires = [
+]
+
+variants = [
+]
+
+def commands():
+    env.REZ_JWT_CPP_ROOT = '{root}'
+    env.PKG_CONFIG_PATH.append("{root}/cmake")
+    env.JWT_CPP_INCLUDE_DIR = "{root}/include"
+
+uuid = 'repository.jwt-cpp'
